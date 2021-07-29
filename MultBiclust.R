8a6f7420d260ed3c1cbdb6c58e0f11d27f3d0d3c
Abhishek Chakraborty <ac.xav89@gmail.com>
2021-07-29 14:18:08 -0400

    Additional code for simulation study 6.4

---
+++ MultBiclust.R
@@ -0,0 +1,77 @@
+#==========================================================================================
+#==========================================================================================
+# This is additional code for the simulation study on Multiplicative Biclusters setting (section 6.4)
+#==========================================================================================
+#==========================================================================================
+
+library(reshape2);library(tidyverse);library(ggpubr)
+
+#==========================================================================================
+# dataset generation
+#==========================================================================================
+set.seed(5)
+u = c(10,9,8,7,6,5,4,3,rep(2,17),rep(0,75))
+v = c(10,-10,8,-8,5,-5,rep(3,5),rep(-3,5),rep(0,34))
+u = u/sqrt(sum(u^2))
+v = v/sqrt(sum(v^2))
+d = 50
+mus = (d*u%*%t(v))
+binaryX = (mus!=0)*1
+X = mus+matrix(rnorm(100*50),100,50)
+X = X-mean(X)
+
+saveRDS(X,"MultBiclust_X.rds")
+
+#==========================================================================================
+# Performing Bayesian biclustering (with real dataset MultBiclust_X.rds) using either the first 
+# or second version (see master codes) of the MCMC sampler. Save the results and plots obtained.
+# The following lines of code can be used to plot the means matrices.
+#==========================================================================================
+setwd(".........")   # set working directory
+load("....RData")   # load saved results
+
+# plot true underlying means matrix
+mus = data.frame(mus)
+plotmus = expand.grid(Rows = rownames(mus), Columns = colnames(mus))
+vec = as.vector(as.matrix(mus))
+plotmus$Z = vec
+
+color = c(rainbow(15,start=0.5,end=0.6)[15:1],"white",rainbow(15,start=0.1,end=0.2)[15:1])
+g.mus = ggplot(plotmus, aes(x = Columns, y = Rows)) + geom_tile(aes(fill = Z)) +
+  theme_bw() +
+  scale_fill_gradientn(colours = color, na.value = "white") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_y_discrete(limits = rev(levels(as.factor(plotmus$Rows)))) + 
+  ggtitle("True underlying means matrix") + 
+  theme(plot.title = element_text(hjust = 0.5))
+g.mus + rremove("xy.text")
+
+# plot means matrix from Bayesian biclustering
+bbc.mus = matrix(NA,nrow=I,ncol=J)
+for(i in 1:I){
+  for(j in 1:J){
+    # change criterion (see paper and master codes)
+    bbc.mus[i,j] = mean(train[id_row_mat[,which.max(avgRowRI)]==id_row_mat[i,which.max(avgRowRI)],
+                         id_col_mat[which.max(avgColRI),]==id_col_mat[which.max(avgColRI),j]])		
+  }
+}
+
+bbc.mus = round(data.frame(bbc.mus),2)
+plotbbc.mus = expand.grid(Rows = rownames(bbc.mus), Columns = colnames(bbc.mus))
+vec = as.vector(as.matrix(bbc.mus))
+plotbbc.mus$Z = vec
+
+color = c(rainbow(15,start=0.5,end=0.6)[15:1],"white",rainbow(15,start=0.1,end=0.2)[15:1])
+g.bbc.mus = ggplot(plotbbc.mus, aes(x = Columns, y = Rows)) + geom_tile(aes(fill = Z)) +
+  theme_bw() +
+  scale_fill_gradientn(colours = color, na.value = "white") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_y_discrete(limits = rev(levels(as.factor(plotbbc.mus$Rows)))) + 
+  ggtitle("Bayesian biclustering") + 
+  theme(plot.title = element_text(hjust = 0.5))
+g.bbc.mus + rremove("xy.text") 
+
+# plot both means matrices on the same plot
+ggarrange(g.mus+rremove("xy.text"), g.bbc.mus+rremove("xy.text"),
+          labels=c("(a)","(b)"),ncol=2,common.legend = TRUE,font.label = list(size=8))
+#==========================================================================================
